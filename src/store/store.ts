93697ce98bb5575488533353e61c913f115980b7
Evgeny Morash <evm037@mail.ru>
2022-09-06 15:25:45 +0300

    init: first commit

---
+++ src/store/store.ts
@@ -0,0 +1,25 @@
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import thunk from "redux-thunk";
+
+
+const rootReducer = combineReducers({
+
+})
+
+export type AppRootState = ReturnType<typeof rootReducer>
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+
+
+
+
+
+
+
+
+
+
+// @ts-ignore
+window.store = store
